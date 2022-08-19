0827a01f9069617cafcb0e4af6fbc29b3c9ec97b
War777 <oscarResendizUaq@gmail.com>
2022-08-19 12:32:13 -0500

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import {Button} from './Button';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 1
+    }
+
+  }
+
+  handleCount(value) {
+    this.setState((prevState) => ({ count: prevState.count + value }));
+  }
+
+  render() {
+
+
+    return (
+      <div>
+        Current count: {this.state.count}
+        <hr />
+        <Button sign="+" count={this.state.count} updateCount={this.handleCount.bind(this)} />
+        <Button sign="-" count={this.state.count} updateCount={this.handleCount.bind(this)} />
+      </div>
+    );
+  }
 }
 
 export default App;
